37cfc04029d728af763c151c0a0a63a59ae7eaf9
MajinDohko <agarridoluengo@gmail.com>
2024-12-16 20:19:08 +0100

    creado archivos

---
+++ osModule.js
@@ -0,0 +1,2 @@
+//Sacar datos: Nombre, Tipo, Versión, Arquitectura, CPUs, Memoria Total, Memoria Libre.
+// Memoria Total y Memoria Libre tendrá que estar en MB. Mira que dato dá y qué cáculo debes hacer
